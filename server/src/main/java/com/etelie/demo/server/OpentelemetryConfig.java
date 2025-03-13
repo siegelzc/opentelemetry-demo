82db726aa88131ba77597dcc3b78577f0f6147eb
siegelzc <siegelzc@gmail.com>
2025-03-13 00:29:38 -0500

    OpentelemetryConfig init

--- server/src/main/java/com/etelie/demo/server/OpentelemetryConfig.java
+++ server/src/main/java/com/etelie/demo/server/OpentelemetryConfig.java
@@ -1,29 +1,47 @@
-import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.api.trace.Tracer;
-import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
-import io.opentelemetry.sdk.OpenTelemetrySdk;
-import io.opentelemetry.sdk.trace.SdkTracerProvider;
-import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+package com.etelie.demo.server;
 
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.logs.SdkLoggerProvider;
+import io.opentelemetry.sdk.metrics.SdkMeterProvider;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.samplers.Sampler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpentelemetryConfig {
 
-    public static OpenTelemetry initOpenTelemetry() {
-        // Create a span exporter (e.g., OTLP exporter for Jaeger, Tempo, etc.)
-        OtlpGrpcSpanExporter spanExporter = OtlpGrpcSpanExporter.builder()
-                .setEndpoint("http://localhost:4317") // OTLP endpoint
-                .build();
-
-        // Create a span processor
-        BatchSpanProcessor spanProcessor = BatchSpanProcessor.builder(spanExporter).build();
-
-        // Create a tracer provider
-        SdkTracerProvider tracerProvider = SdkTracerProvider.builder()
-                .addSpanProcessor(spanProcessor)
-                .build();
-
-        // Build the OpenTelemetry instance
+    @Bean
+    public OpenTelemetry initOpenTelemetry(
+            SdkLoggerProvider sdkLoggerProvider,
+            SdkTracerProvider sdkTracerProvider,
+            SdkMeterProvider sdkMeterProvider
+    ) {
         return OpenTelemetrySdk.builder()
-                .setTracerProvider(tracerProvider)
+                .setLoggerProvider(sdkLoggerProvider)
+                .setTracerProvider(sdkTracerProvider)
+                .setMeterProvider(sdkMeterProvider)
                 .buildAndRegisterGlobal();
     }
+
+    @Bean
+    public SdkLoggerProvider openTelemetryLoggerProvider() {
+        return SdkLoggerProvider.builder()
+                .build();
+    }
+
+    @Bean
+    public SdkTracerProvider openTelemetryTracerProvider() {
+        return SdkTracerProvider.builder()
+                .setSampler(Sampler.alwaysOn())
+                .build();
+    }
+
+    @Bean
+    public SdkMeterProvider openTelemetryMeterProvider() {
+        return SdkMeterProvider.builder()
+                .build();
+    }
+
 }
