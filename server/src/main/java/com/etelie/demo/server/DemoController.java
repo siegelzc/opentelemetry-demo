068aedda754af42df0e24eb244b77edcc723eb52
siegelzc <siegelzc@gmail.com>
2025-03-16 18:25:04 -0500

    server instrumentation

--- server/src/main/java/com/etelie/demo/server/DemoController.java
+++ server/src/main/java/com/etelie/demo/server/DemoController.java
@@ -1,9 +1,19 @@
 package com.etelie.demo.server;
 
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.context.propagation.TextMapGetter;
+import io.opentelemetry.context.propagation.TextMapSetter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,16 +22,49 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/demo")
 public class DemoController {
 
-    Logger logger = LoggerFactory.getLogger(DemoController.class);
+    private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
+
+    private final ContextPropagators contextPropagators;
+    private final TextMapGetter<HttpHeaders> httpHeadersGetter;
+    private final TextMapSetter<HttpHeaders> httpHeadersSetter;
+    private final Tracer tracer;
+
+    public DemoController(
+            ContextPropagators contextPropagators,
+            TextMapGetter<HttpHeaders> httpHeadersGetter,
+            TextMapSetter<HttpHeaders> httpHeadersSetter,
+            Tracer tracer
+    ) {
+        this.contextPropagators = contextPropagators;
+        this.httpHeadersGetter = httpHeadersGetter;
+        this.httpHeadersSetter = httpHeadersSetter;
+        this.tracer = tracer;
+    }
 
     @RequestMapping(path = "/hello", method = RequestMethod.GET)
     public ResponseEntity<String> hello(
-            @RequestParam("target") String target
+            @RequestParam("target") String target,
+            @RequestHeader HttpHeaders headers
     ) {
-        String message = "Hello %s!".formatted(target);
+        Context extractedContext = contextPropagators.getTextMapPropagator()
+                .extract(Context.current(), headers, httpHeadersGetter);
 
-        logger.debug(message);
-        return ResponseEntity.ok().body(message);
+        try (Scope ignored = extractedContext.makeCurrent()) {
+            Span span = tracer.spanBuilder("hello")
+                    .setSpanKind(SpanKind.SERVER)
+                    .setAttribute("target", target)
+                    .startSpan();
+
+            String message = "Hello %s!".formatted(target);
+            logger.debug(message);
+
+            span.setAttribute("message", message);
+            HttpHeaders responseHeaders = new HttpHeaders();
+            contextPropagators.getTextMapPropagator()
+                    .inject(Context.current(), responseHeaders, httpHeadersSetter);
+
+            return ResponseEntity.ok().headers(responseHeaders).body(message);
+        }
     }
 
 }
--- server/src/main/java/com/etelie/demo/server/OpentelemetryConfig.java
+++ server/src/main/java/com/etelie/demo/server/OpentelemetryConfig.java
@@ -2,6 +2,13 @@ package com.etelie.demo.server;
 
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.baggage.propagation.W3CBaggagePropagator;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.context.propagation.TextMapGetter;
+import io.opentelemetry.context.propagation.TextMapPropagator;
+import io.opentelemetry.context.propagation.TextMapSetter;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.logs.SdkLoggerProvider;
 import io.opentelemetry.sdk.metrics.SdkMeterProvider;
@@ -9,6 +16,7 @@ import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.samplers.Sampler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
 @Configuration
 public class OpentelemetryConfig {
@@ -24,20 +32,63 @@ public class OpentelemetryConfig {
         return openTelemetry;
     }
 
-    public SdkLoggerProvider loggerProvider() {
+    private SdkLoggerProvider loggerProvider() {
         return SdkLoggerProvider.builder()
                 .build();
     }
 
-    public SdkTracerProvider tracerProvider() {
+    private SdkTracerProvider tracerProvider() {
         return SdkTracerProvider.builder()
                 .setSampler(Sampler.alwaysOn())
                 .build();
     }
 
-    public SdkMeterProvider meterProvider() {
+    private SdkMeterProvider meterProvider() {
         return SdkMeterProvider.builder()
                 .build();
+    }
+
+    @Bean
+    public ContextPropagators contextPropagators() {
+        return ContextPropagators.create(
+                TextMapPropagator.composite(
+                        W3CTraceContextPropagator.getInstance(),
+                        W3CBaggagePropagator.getInstance()
+                )
+        );
+    }
+
+    @Bean
+    public TextMapGetter<HttpHeaders> httpHeadersGetter() {
+        return new TextMapGetter<>() {
+            @Override
+            public Iterable<String> keys(HttpHeaders carrier) {
+                return carrier.keySet();
+            }
+
+            @Override
+            public String get(HttpHeaders carrier, String key) {
+                return carrier.asSingleValueMap().get(key);
+            }
+        };
+    }
+
+    @Bean
+    public TextMapSetter<HttpHeaders> httpHeadersSetter() {
+        //noinspection Convert2Lambda,Anonymous2MethodRef
+        return new TextMapSetter<>() {
+            @Override
+            public void set(HttpHeaders httpHeaders, String headerName, String headerValue) {
+                httpHeaders.set(headerName, headerValue);
+            }
+        };
+    }
+
+    @Bean
+    public Tracer tracer(
+            OpenTelemetry openTelemetry
+    ) {
+        return openTelemetry.getTracer(ServerApplication.class.getPackageName());
     }
 
 }
